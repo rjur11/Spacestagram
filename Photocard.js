e4db1d49b20ffb9d8adb3136002e11ce4ed0be54
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-01-17 20:06:09 -0500

    Using photocard class to render page

--- Photocard.js
+++ Photocard.js
@@ -10,4 +10,44 @@ export default class Photocard {
   toggleLike() {
     this.liked = !this.liked;
   }
+
+  #createImg() {
+    const img = document.createElement("img");
+    img.src = this.url;
+    img.alt = this.title;
+    return img;
+  }
+
+  #createTitleBar() {
+    const titleBar = document.createElement("h2");
+    titleBar.innerText = `${this.title} - ${this.date}`;
+    return titleBar;
+  }
+
+  #createDescription() {
+    const description = document.createElement("p");
+    description.innerText = this.explanation;
+    return description;
+  }
+
+  #createLikeButton() {
+    const likeButton = document.createElement("button");
+    likeButton.innerText = this.liked ? "Unlike" : "Like";
+    likeButton.addEventListener("click", () => {
+      this.toggleLike();
+      likeButton.innerText = this.liked ? "Unlike" : "Like";
+    });
+    return likeButton;
+  }
+
+  getElement() {
+    const article = document.createElement("article");
+    article.classList.add("photo-card");
+    article.appendChild(this.#createImg());
+    article.appendChild(this.#createTitleBar());
+    article.appendChild(this.#createDescription());
+    article.appendChild(this.#createLikeButton());
+    console.log(article.innerHTML);
+    return article;
+  }
 }
--- scripts.js
+++ scripts.js
@@ -1,57 +1,40 @@
 const photoCardSection = document.querySelector(".daily-cards");
 const loader = document.querySelector(".rocket-ship");
 import { fetchData } from "../apiCalls.js";
+import Photocard from "./Photocard.js";
+
+const addEntriesToPage = (entries) => {
+  entries
+    .map((entry) => new Photocard(entry))
+    .forEach((photocard) => {
+      photoCardSection.appendChild(photocard.getElement());
+    });
+};
+
+const createScrollCallback = () => {
+  let isRunning = false;
+  return () => {
+    console.log(isRunning);
+    if (isRunning) {
+      return;
+    }
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (clientHeight + scrollTop >= scrollHeight - 5) {
+      isRunning = true;
+      loader.classList.remove("hidden");
+      fetchData().then((entries) => {
+        setTimeout(() => {
+          addEntriesToPage(entries);
+          isRunning = false;
+          loader.classList.add("hidden");
+        }, 1000);
+      });
+    }
+  };
+};
 
 window.addEventListener("load", () => {
-  fetchData().then((entries) => {
-    entries.forEach((entry) => {
-      photoCardSection.appendChild(createPhotoCard(entry));
-    });
-  });
+  fetchData().then(addEntriesToPage);
 });
 
-window.addEventListener("scroll", () => {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-  if (clientHeight + scrollTop >= scrollHeight - 5) {
-    loader.classList.remove("hidden");
-    fetchData().then((entries) => {
-      setTimeout(() => {
-        entries.forEach((entry) => {
-          photoCardSection.appendChild(createPhotoCard(entry));
-        });
-        loader.classList.add("hidden");
-      }, 1000);
-    });
-  }
-});
-
-const createPhotoCard = (entry) => {
-  const { url, title, date, explanation } = entry;
-  const photoCardArticle = document.createElement("article");
-  photoCardArticle.classList.add("photo-card");
-  const img = document.createElement("img");
-  img.src = url;
-  img.alt = title;
-  photoCardArticle.appendChild(img);
-  const titleBar = document.createElement("h2");
-  titleBar.innerText = `${title} - ${date}`;
-  photoCardArticle.appendChild(titleBar);
-  const description = document.createElement("p");
-  description.innerText = explanation;
-  photoCardArticle.appendChild(description);
-  const likeButton = document.createElement("button");
-  likeButton.innerText = "Like";
-  likeButton.addEventListener("click", () => {
-    toggleLikes(likeButton);
-  });
-  photoCardArticle.appendChild(likeButton);
-  return photoCardArticle;
-};
-
-const toggleLikes = (selectedButton) => {
-  if (selectedButton.innerText === "Like") {
-    selectedButton.innerText = "Unlike";
-  } else if (selectedButton.innerText === "Unlike") {
-    selectedButton.innerText = "Like";
-  }
-};
+window.addEventListener("scroll", createScrollCallback());
